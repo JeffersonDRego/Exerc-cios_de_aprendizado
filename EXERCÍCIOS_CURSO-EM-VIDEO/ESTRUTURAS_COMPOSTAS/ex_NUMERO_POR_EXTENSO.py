4e942fbde67f03ff68398427ff3f0b3824a9d8cb
JeffersonDRego <jeff.drsp@gmail.com>
2021-08-23 15:27:27 -0300

    Update ex_NUMERO_POR_EXTENSO.py
    CORRIGINDO CRASH POR ERROS DE DIGITAÇÂO DO USUÁRIO.

--- EXERCÍCIOS_CURSO-EM-VIDEO/ESTRUTURAS_COMPOSTAS/ex_NUMERO_POR_EXTENSO.py
+++ EXERCÍCIOS_CURSO-EM-VIDEO/ESTRUTURAS_COMPOSTAS/ex_NUMERO_POR_EXTENSO.py
@@ -12,8 +12,6 @@ while True:
 for pos, c in enumerate(numeros):
     print (numeros[2])
     '''
-from typing import Counter, Literal, Tuple
-
 
 numeros=(1,2,3,4,5,6,7,8,9,10)
 numeros_ext=('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez')
@@ -22,7 +20,12 @@ numeros_ext=('zero','um','dois','três','quatro','cinco','seis','sete','oito','n
 
 
 while True:
-    n=int(input('\033[1;32mDigite um número de 0 a 10.\n\033[m'))
+    while True:
+        try:
+            n=int(input('\033[1;32mDigite um número de 0 a 10.\n\033[m'))
+            break
+        except ValueError:
+            print ('***DIGITE APENAS UM NÚMERO DE 1 A 10***')
     if 0<=n<=10:
         print(f'Você digitou {n} que se escreve {numeros_ext[n]}')
         continuar=str(input('Quer continuar? [S/N]\n')) .upper() .strip()
